231ee1f0c673bcc3372d79ebe9750862d285aa79
jpauloasx <joaocerqueira@cbm.mt.gov.br>
2025-09-27 12:15:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def home():
 # Campos do formulário
 campos = [
     ("Nº do Laudo", "numero_laudo"),
+    ("OS, Ofício ou Processo", "n_processo")
     ("Nome", "nome"),
     ("CPF", "cpf"),
     ("Telefone", "telefone"),
@@ -213,6 +214,7 @@ def logout():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
