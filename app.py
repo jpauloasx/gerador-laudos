a186dc83883e3d0400f1550cdc33307397fbaf48
jpauloasx <joaocerqueira@cbm.mt.gov.br>
2025-10-04 15:09:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-ffrom flask import Flask, render_template, request, send_file, redirect, url_for, session
+from flask import Flask, render_template, request, send_file, redirect, url_for, session
 from docxtpl import DocxTemplate, InlineImage
 from docx.shared import Mm
 from datetime import date
@@ -373,6 +373,7 @@ def logout():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
