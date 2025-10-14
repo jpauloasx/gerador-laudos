05816f0026e69367cb4de1504ad01a69cda4c2ce
jpauloasx <joaocerqueira@cbm.mt.gov.br>
2025-10-14 16:31:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import (
-    Flask, render_template, request, redirect, url_for, session, send_file
+    Flask, render_template, request, redirect, url_for, session, send_file, jsonify
 )
 from docxtpl import DocxTemplate, InlineImage
 from docx.shared import Mm
@@ -479,6 +479,7 @@ def inserir_atendimento():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 
