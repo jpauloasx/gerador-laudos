455a3d169822aaf1322f2e88f6c9c338d3a653e6
jpauloasx <joaocerqueira@cbm.mt.gov.br>
2025-11-29 11:11:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import (
     Flask, render_template, request, redirect, url_for, session, send_file, jsonify
 )
+import requests
 from docxtpl import DocxTemplate, InlineImage
 from docx.shared import Mm
 from datetime import date, datetime
@@ -541,6 +542,7 @@ def inserir_atendimento():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 
